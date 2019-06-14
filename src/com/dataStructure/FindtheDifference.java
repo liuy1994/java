ac8728bf9489994abf0633d7a69eaf41b0f5e408
liuy1994 <liuy1994g@gmail.com>
2019-06-14 11:04:36 +0800

    nrew

---
+++ src/com/dataStructure/FindtheDifference.java
@@ -0,0 +1,10 @@
+package com.dataStructure;
+
+public class FindtheDifference {
+    public static void main(String[] args) {
+        System.out.println(getDiff());
+    }
+    private static String getDiff() {
+        return "abc";
+    };
+}
