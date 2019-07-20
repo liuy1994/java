bc4c97b2b0082104f29a0762de9854364fd45d93
Liu Yong <liuy1994@outlook.com>
2019-07-21 00:07:50 +0800

    's'

--- src/com/dataStructure/Main.java
+++ src/com/dataStructure/Main.java
@@ -4,8 +4,14 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        HanoiTower tower = new HanoiTower(3);
-
+        MinStack stack = new MinStack();
+        stack.push(3);
+        stack.push(5);
+        stack.push(1);
+        System.out.println(stack.top());
+        System.out.println(stack.getMin());
+        stack.pop();
+        System.out.println(stack.getMin());
     }
 }
 
