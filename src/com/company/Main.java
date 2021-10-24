45caed4278e5167236eae7bfb5523d3f71310026
liuy1994 <liuy1994@outlook.com>
2021-10-24 14:31:59 +0800

    io

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,21 +3,54 @@ package com.company;
 import com.company.Human;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        PrintStream output = System.out;
-        output.println("hello word");
 
-        InputStream input = System.in;
 
-        InputStreamReader reader =  new InputStreamReader(input);
+        InputStreamReader reader1 =  new InputStreamReader(System.in);
 
         char[] buffer = new char[20];
-        int length = reader.read(buffer);
+        int length = reader1.read(buffer);
 
-        output.println(length);
+        System.out.println(length);
+        System.out.println(buffer);
+
+        // 从一个文件读取内容，输出到另一个文件
+
+        FileInputStream input = new FileInputStream("res/input.txt");
+        FileOutputStream output = new FileOutputStream("res/output.txt");
+
+        InputStreamReader reader = new InputStreamReader(input, StandardCharsets.UTF_8);
+        OutputStreamWriter writer = new OutputStreamWriter(output, StandardCharsets.UTF_8);
+
+        BufferedReader bufferedReader = new BufferedReader(reader);
+        PrintWriter printer = new PrintWriter(writer);
+
+//        bufferredReader.readLine();
+
+//        String context = new String();
+//        for (String line = bufferedReader.readLine(); line != null; line = bufferedReader.readLine()) {
+//            // 处理这一行
+//            context = context + line;
+//
+//        }
+
+        String context = "";
+        String line = null;
+        while ((line = bufferedReader.readLine()) != null) {
+            context += line + "\n";
+        }
+
+        reader.close();
+        input.close();
+        System.out.println(context);
+        printer.print(context);
+        printer.close();
+        output.close();
+
     }
 }
