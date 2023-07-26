f5bebb1085cf71c6a32c51007ebc451c33b36c98
liam <liam.liu@wayz.ai>
2023-07-26 16:31:59 +0800

    inputStream

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,34 @@
 package com.company;
+import java.nio.charset.*;
 import java.util.*;
 import java.io.*;
 
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        InputStreamReader reader = new InputStreamReader(System.in);
-        char[] buffer = new char[20];
-        int length = reader.read(buffer);
-        System.out.println(length);
+        FileInputStream input = new FileInputStream("res/input.txt");
+        FileOutputStream output = new FileOutputStream("res/output.txt");
+
+        InputStreamReader reader = new InputStreamReader(input, StandardCharsets.UTF_8);
+        OutputStreamWriter writer = new OutputStreamWriter(output, StandardCharsets.UTF_8);
+
+        BufferedReader buffer = new BufferedReader(reader);
+        PrintWriter print = new PrintWriter(writer);
+
+        String content = "";
+        String line = buffer.readLine();
+        while (line != null) {
+            content += line + "\n";
+            line = buffer.readLine();
+        }
+        buffer.close();
+        reader.close();
+        input.close();
+        System.out.println(content);
+        print.print(content);
+        print.close();
+        writer.close();
+        output.close();
     }
 }
 
