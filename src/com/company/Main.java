67d8ebc5d405963a781573f50d0c74085f0c0b0f
liam <liam.liu@wayz.ai>
2023-07-28 16:19:34 +0800

    快速排序

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,45 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-      System.out.println(isValid("[][][]"));
+//      System.out.println(isValid("[][][]"));
+//      Queue<Integer> queue = new LinkedList<Integer>();
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        String str = "23654";
+
+        for (String s : str.split("")) {
+            list.add(Integer.parseInt(s));
+        }
+        System.out.println(list);
+        sort(list, 0, list.size() - 1);
+
+
+        System.out.println(list);
+    }
+
+    public static void sort(ArrayList<Integer> list, Integer left, Integer right) {
+        int i = left;
+        int j = right;
+        int temp = list.get(left);
+        while (i < j) {
+            while (i < j && list.get(i) < temp) {
+                i++;
+            }
+            while (i < j && list.get(j) > temp) {
+                j--;
+            }
+            if (i < j) {
+                int mid = list.get(i);
+                list.set(i, list.get(j));
+                list.set(j, mid);
+            }
+        }
+        if (left < i) {
+            sort(list, left, i - 1);
+        }
+        if (i < right) {
+            sort(list, i + 1, right);
+        }
     }
 
     public static Boolean isValid(String str) {
