9be6f591a5d6f2661a92faa5096078264586c999
liam <liam.liu@wayz.ai>
2023-08-10 14:16:53 +0800

    发送一个http请求

--- src/main/java/spring/user/UserController.java
+++ src/main/java/spring/user/UserController.java
@@ -1,9 +1,15 @@
 package spring.user;
 
+import org.apache.tomcat.util.json.JSONParser;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -25,9 +31,12 @@ public class UserController {
     }
 
     @PostMapping("/user")
-    User createUser(@RequestBody User user){
+    ResponseEntity<User> createUser(@RequestBody User user){
+        if (users.containsKey(user.getName())){
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
         users.put(user.getName(), user);
-        return user;
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
     }
     @DeleteMapping("/user/{name}")
     ResponseEntity<Void> deleteUser(@PathVariable String name){
@@ -43,6 +52,23 @@ public class UserController {
             User result = users.get(user.getName());
             result.setContact(user.getContact());
             return new ResponseEntity<>(users.get(user.getName()), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+    @GetMapping("/http")
+    ResponseEntity<String> http() throws IOException {
+        URL url = new URL("https://api.readhub.cn/stock_file/search_key/recommend/list?type=100&web=true");
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+
+//读取响应
+        BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+        String inputLine = in.readLine();
+
+        in.close();
+        if (inputLine != null){
+            return new ResponseEntity<>(inputLine, HttpStatus.OK);
         }
         return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
